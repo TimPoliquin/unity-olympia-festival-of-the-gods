76f45f8337801e78baaebe51a270e47679ac0d11
Tim Poliquin <tim.poliquin@gmail.com>
2024-08-04 17:15:30 -0400

    IN PROGRESS - Input Controller

--- Assets/Scripts/Controllers/FactoryController.cs
+++ Assets/Scripts/Controllers/FactoryController.cs
@@ -36,16 +36,17 @@ namespace Azul
             {
                 foreach (Factory factory in this.factories)
                 {
-                    factory.Fill(bagController.Draw(4));
+                    List<Tile> tiles = bagController.Draw(4);
+                    tiles.ForEach(tile =>
+                    {
+                        // TODO - this doesn't feel like it belongs in the FACTORY controller.
+                        TilePointerController tilePointerController = tile.GetComponent<TilePointerController>();
+                        tilePointerController.AddOnTileHoverEnterListener(this.OnTileHoverEnter);
+                        tilePointerController.AddOnTileHoverExitListener(this.OnTileHoverExit);
+                        tilePointerController.AddOnTileSelectListener(this.OnTileSelect);
+                    });
+                    factory.Fill(tiles);
                 }
-            }
-
-            GameObject CreateFactory(string name)
-            {
-                GameObject gameObject = Instantiate(this.factoryPrefab);
-                gameObject.name = name;
-                this.factories.Add(gameObject.GetComponent<Factory>());
-                return gameObject;
             }
 
             public List<Factory> GetFactories()
@@ -53,9 +54,33 @@ namespace Azul
                 return this.factories;
             }
 
+            private GameObject CreateFactory(string name)
+            {
+                GameObject gameObject = Instantiate(this.factoryPrefab);
+                gameObject.name = name;
+                this.factories.Add(gameObject.GetComponent<Factory>());
+                return gameObject;
+            }
+
             private void OnRoundStart(OnRoundPhasePreparePayload payload)
             {
                 this.FillFactories(System.Instance.GetBagController());
+            }
+
+            private void OnTileHoverEnter(OnTileHoverEnterPayload payload)
+            {
+                UnityEngine.Debug.Log("Hovering tile");
+            }
+
+            private void OnTileHoverExit(OnTileHoverExitPayload payload)
+            {
+                UnityEngine.Debug.Log("Exiting tile");
+
+            }
+
+            private void OnTileSelect(OnTileSelectPayload payload)
+            {
+                UnityEngine.Debug.Log("Selecing tile");
             }
         }
     }
